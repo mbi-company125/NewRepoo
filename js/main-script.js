db7cd53e8d4624e7e5a1ce5ad19dfd4a78f290a8
Mos3b boshra <mosabboshra1998@gmail.com>
2021-03-20 16:54:52 +0200

    added

--- js/main-script.js
+++ js/main-script.js
@@ -33,18 +33,43 @@ $(function () {
 });**/
 
 // Validate Column
-function validate()
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.oddEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs()
 {
-    if(document.contactform.name.value=="")
+
+    // Git the value from the input
+
+    const nusernameValue = nusername.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(uusernameValue === '')
     {
-        alert("الرجاء إدخال إسمك")
+        setErrorFor(username, 'username cannot be balnk')
     }
-    else if(document.contactform.tel.value=="")
-    {
-        alert("الرجاء إدخال موبايلك")
+    else{
+        setSuccessFor(username)
     }
-    else if(document.contactform.email.value=="")
-    {
-        alert("الرجاء إدخال ايملك")
-    }
+}
+
+function setErrorFor(input, message)
+{
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = "form control error";
 }
